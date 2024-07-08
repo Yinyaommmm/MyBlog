c0929bc7877020038803a5015307ef644f876fe6
Wei Qingqv <zjcxwqq@126.com>
2024-07-08 14:37:04 +0800

    configmodule and typeormmodule

--- backend_blog/src/app.controller.ts
+++ backend_blog/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    console.log(this.configService.get('DATABASE_URL'));
+    console.log(process.env.DATABASE_URL);
+
+    return {
+      a: 1,
+    };
   }
 }
--- backend_blog/src/app.module.ts
+++ backend_blog/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_URL,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // 同步迁移
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
